2175fdf4da28473525ac686330cc8b99147f9e82
Rumeysa <michiru.aware.lrn@gmail.com>
2024-03-11 02:55:45 +0300

    Initial Commit

---
+++ AlertMessages/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlertMessages
+//
+//  Created by Aydın Kutlu on 11.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
